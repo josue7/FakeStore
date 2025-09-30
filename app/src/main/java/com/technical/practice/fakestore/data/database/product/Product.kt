96469503d2cf7f8b8ee4fdc3bf4d45d3e0e0900d
Josue Marfil <josue.marfil@estudiante.uacm.edu.mx>
2025-09-30 17:27:39 -0600

    Agrego claves foráneas
    Se agrego las claves foráneas de las entidades Favorite y Product

--- app/src/main/java/com/technical/practice/fakestore/data/database/product/Product.kt
+++ app/src/main/java/com/technical/practice/fakestore/data/database/product/Product.kt
@@ -2,9 +2,22 @@ package com.technical.practice.fakestore.data.database.product
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import com.technical.practice.fakestore.data.database.category.Category
 
-@Entity (tableName = "product")
+@Entity (
+    tableName = "product",
+    foreignKeys = [
+        ForeignKey (
+            entity = Category::class,
+            parentColumns = ["id_category"],
+            childColumns = ["category_id"],
+            onDelete = ForeignKey.CASCADE
+
+        )
+    ]
+)
 data class Product (
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id_product") val idProduct: Int = 1,
     val title: String,
