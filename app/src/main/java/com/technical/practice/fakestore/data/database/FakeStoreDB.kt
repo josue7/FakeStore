a1c3bfab8bc924d88cc974ecdbaf2324245341ed
Josue Marfil <josue.marfil@estudiante.uacm.edu.mx>
2025-09-30 20:24:32 -0600

    Se elimino favorite
    Se pensó distinto la base de datos por lo que Favorite ya no se usa

--- app/src/main/java/com/technical/practice/fakestore/data/database/FakeStoreDB.kt
+++ app/src/main/java/com/technical/practice/fakestore/data/database/FakeStoreDB.kt
@@ -6,16 +6,13 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.technical.practice.fakestore.data.database.category.Category
 import com.technical.practice.fakestore.data.database.category.CategoryDao
-import com.technical.practice.fakestore.data.database.favorite.Favorite
-import com.technical.practice.fakestore.data.database.favorite.FavoriteDao
 import com.technical.practice.fakestore.data.database.product.Product
 import com.technical.practice.fakestore.data.database.product.ProductDao
 
-@Database ( entities = [Category::class, Product::class, Favorite::class], version = 1, exportSchema = false )
+@Database ( entities = [Category::class, Product::class], version = 1, exportSchema = false )
 abstract class FakeStoreDB: RoomDatabase() {
     abstract fun categoryDao(): CategoryDao
     abstract fun productDao(): ProductDao
-    abstract fun favoriteDao(): FavoriteDao
 
     companion object {
         @Volatile
