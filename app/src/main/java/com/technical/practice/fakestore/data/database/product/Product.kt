5fdbfc096a568a8bf0c2d8f26faf269ca86104b2
Josue Marfil <josue.marfil@estudiante.uacm.edu.mx>
2025-09-30 19:51:30 -0600

    nuevo atributo
    se agrego un nuevo atributo a la tabla

--- app/src/main/java/com/technical/practice/fakestore/data/database/product/Product.kt
+++ app/src/main/java/com/technical/practice/fakestore/data/database/product/Product.kt
@@ -22,7 +22,8 @@ data class Product (
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id_product") val idProduct: Int = 1,
     val title: String,
     val price: Float,
-     val description: String,
+    val description: String,
     @ColumnInfo(name = "category_id") val categoryID: Int,
-    val image: String
+    val image: String,
+    val isFavorite: Boolean = false
 )
