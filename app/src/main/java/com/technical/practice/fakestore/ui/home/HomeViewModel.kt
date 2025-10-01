5c16f1bb163d4fcc2aa0abf08cc365502d3fac91
Josue Marfil <josue.marfil@estudiante.uacm.edu.mx>
2025-10-01 10:56:47 -0600

    Creación de HomeViewModel
    Se creo HomeViewModel para obtener los datos y mostrarlos en las pantallas de la app

--- app/src/main/java/com/technical/practice/fakestore/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/technical/practice/fakestore/ui/home/HomeViewModel.kt
@@ -1,8 +1,47 @@
 package com.technical.practice.fakestore.ui.home
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.technical.practice.fakestore.data.apiservice.ProductAPI
+import com.technical.practice.fakestore.data.repository.ProductRepositoryNet
+import kotlinx.coroutines.launch
+import okio.IOException
 
-class HomeViewModel (application: Application): AndroidViewModel (application) {
+sealed interface ProductUiState {
+    data class Success(val products: List<ProductAPI>) : ProductUiState
+    object Error : ProductUiState
+    object Loading : ProductUiState
+}
+
+class HomeViewModel (
+    private val productRepositoryNet: ProductRepositoryNet
+): ViewModel () {
+    var productUiState: ProductUiState by mutableStateOf(ProductUiState.Loading)
+        private set
+
+    private lateinit var products: List<ProductAPI>
+    private lateinit var categories: List<String>
+
+    init {
+        getProducts()
+    }
+
+    fun getProducts () {
+        viewModelScope.launch {
+            productUiState = ProductUiState.Loading
+            productUiState = try {
+                ProductUiState.Success(productRepositoryNet.getProducts())
+            } catch (e: IOException) {
+                ProductUiState.Error
+            } catch (e: HttpException) {
+                ProductUiState.Error
+            }
+        }
+    }
 
 }
