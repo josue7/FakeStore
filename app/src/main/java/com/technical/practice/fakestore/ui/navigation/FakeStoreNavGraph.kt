a6721cd5c83bacda2fbc1ac578d75f3bea0ee6b6
Josue Marfil <josue.marfil@estudiante.uacm.edu.mx>
2025-09-30 22:40:38 -0600

    Creación de FakeStoreNavGraph
    Este archivo se creo para agregar la pila de pantallas las cuales navegara el usuario

---
+++ app/src/main/java/com/technical/practice/fakestore/ui/navigation/FakeStoreNavGraph.kt
@@ -0,0 +1,24 @@
+package com.technical.practice.fakestore.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.technical.practice.fakestore.ui.home.HomeDestination
+
+@Composable
+fun FakeStoreNavGraph (
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = HomeDestination.route,
+        modifier = modifier
+    ) {
+        composable ( route = HomeDestination.route ) {
+
+        }
+    }
+}
