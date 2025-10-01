8ca360d84b67bfc0f648f61306320e1e1a97bb4c
Josue Marfil <josue.marfil@estudiante.uacm.edu.mx>
2025-10-01 10:49:19 -0600

    Se cambio de objetivo
    Se quito el uso para serializacion de API por solo uso con la DB

--- app/src/main/java/com/technical/practice/fakestore/data/database/product/Product.kt
+++ app/src/main/java/com/technical/practice/fakestore/data/database/product/Product.kt
@@ -4,32 +4,21 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
-import com.technical.practice.fakestore.data.database.category.Category
+//import com.technical.practice.fakestore.data.database.category.Category
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-@Serializable
 @Entity (
-    tableName = "product",
-    foreignKeys = [
-        ForeignKey (
-            entity = Category::class,
-            parentColumns = ["id_category"],
-            childColumns = ["category_id"],
-            onDelete = ForeignKey.CASCADE
-
-        )
-    ]
+    tableName = "product"
 )
 data class Product (
-    @PrimaryKey(autoGenerate = true)
+    @PrimaryKey()
     @ColumnInfo(name = "id_product")
-    @SerialName(value = "id")
-    val idProduct: Int = 1,
-    @SerialName(value = "title") val title: String,
-    @SerialName (value = "price") val price: Float,
-    @SerialName(value = "description") val description: String,
-    @ColumnInfo(name = "category_id") @SerialName(value = "category") val categoryID: Int,
-    @SerialName(value = "image") val image: String,
+    val idProduct: Int,
+    val title: String,
+    val price: Double,
+    val description: String,
+    val category: String,
+    val image: String,
     @Transient @ColumnInfo(name = "is_favorite") val isFavorite: Boolean = false
 )
