b9edd203737292378989e68cc95801b7e2dd313c
Josue Marfil <josue.marfil@estudiante.uacm.edu.mx>
2025-10-01 10:39:41 -0600

    Cambio de biblioteca
    Se cambio de biblioteca ya que no convertía bien el Json

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     // Retrofit
     implementation(libs.retrofit)
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation (libs.androidx.navigation.compose)
     implementation("io.coil-kt:coil-compose:2.7.0")
