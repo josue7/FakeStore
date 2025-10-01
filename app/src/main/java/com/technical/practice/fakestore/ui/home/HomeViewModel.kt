59d65d416257f5b6addd55c98a923bd16972a861
Josue Marfil <josue.marfil@estudiante.uacm.edu.mx>
2025-10-01 00:25:15 -0600

    Creación de HomeViewModel
    Se creo la clase ViewModel de Home

---
+++ app/src/main/java/com/technical/practice/fakestore/ui/home/HomeViewModel.kt
@@ -0,0 +1,8 @@
+package com.technical.practice.fakestore.ui.home
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class HomeViewModel (application: Application): AndroidViewModel (application) {
+
+}
