0b290d733d8dee99d436dba9ab88f0195caf9675
Josue Marfil <josue.marfil@estudiante.uacm.edu.mx>
2025-10-01 10:54:47 -0600

    Creación de AppViewModelProvider
    Se creo la case que contiene un Factory para crear instancias de viewModel que se usaran

---
+++ app/src/main/java/com/technical/practice/fakestore/ui/AppViewModelProvider.kt
@@ -0,0 +1,20 @@
+package com.technical.practice.fakestore.ui
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.technical.practice.fakestore.FakeStoreApplication
+import com.technical.practice.fakestore.data.apiservice.FakeStoreApiService
+import com.technical.practice.fakestore.ui.home.HomeViewModel
+
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(productRepositoryNet = this.fakeStoreApplication().container.productRepositoryNet)
+        }
+    }
+
+}
+
+fun CreationExtras.fakeStoreApplication(): FakeStoreApplication = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as FakeStoreApplication)
