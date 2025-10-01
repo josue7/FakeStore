a5b14fe36d0eff513d5dfbfb915b3f92739d0d1f
Josue Marfil <josue.marfil@estudiante.uacm.edu.mx>
2025-10-01 17:09:59 -0600

    Cambio mínimo en mapeado
    Se agrego los ColumnInfo para darle un buen mapeo a la base de datos, además de quitar importaciones que no usare

--- app/src/main/java/com/technical/practice/fakestore/data/database/product/Product.kt
+++ app/src/main/java/com/technical/practice/fakestore/data/database/product/Product.kt
@@ -2,23 +2,17 @@ package com.technical.practice.fakestore.data.database.product
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
-//import com.technical.practice.fakestore.data.database.category.Category
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
 
 @Entity (
     tableName = "product"
 )
 data class Product (
-    @PrimaryKey()
-    @ColumnInfo(name = "id_product")
-    val idProduct: Int,
-    val title: String,
-    val price: Double,
-    val description: String,
-    val category: String,
-    val image: String,
-    @Transient @ColumnInfo(name = "is_favorite") val isFavorite: Boolean = false
+    @PrimaryKey @ColumnInfo(name = "id_product") val idProduct: Int,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "price") val price: Double,
+    @ColumnInfo(name = "description") val description: String,
+    @ColumnInfo(name = "category") val category: String,
+    @ColumnInfo(name = "image") val image: String,
+    @ColumnInfo(name = "is_favorite") val isFavorite: Boolean = false
 )
