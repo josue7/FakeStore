3e1b398229abea25fb96422bf531d055fc66b25b
Josue Marfil <josue.marfil@estudiante.uacm.edu.mx>
2025-09-30 22:43:11 -0600

    Cambio de pantalla de inicio
    Se cambio el archivo que abrirá al iniciar la app

--- app/src/main/java/com/technical/practice/fakestore/MainActivity.kt
+++ app/src/main/java/com/technical/practice/fakestore/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.technical.practice.fakestore.ui.FakeStoreApp
 import com.technical.practice.fakestore.ui.theme.FakeStoreTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FakeStoreTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                }
+                FakeStoreApp()
             }
         }
     }
@@ -31,5 +25,5 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun MainPreview() {
-
+    FakeStoreApp()
 }
