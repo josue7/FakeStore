73581449533f11ca20941873d3930b8ab061dc67
Josue Marfil <josue.marfil@estudiante.uacm.edu.mx>
2025-09-30 18:21:36 -0600

    Falto anotación
    Se agrego anotación faltante

--- app/src/main/java/com/technical/practice/fakestore/data/database/product/ProductDao.kt
+++ app/src/main/java/com/technical/practice/fakestore/data/database/product/ProductDao.kt
@@ -1,8 +1,10 @@
 package com.technical.practice.fakestore.data.database.product
 
+import androidx.room.Dao
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface ProductDao {
     @Query ("SELECT * FROM product WHERE category_id = :productID")
     fun getProductByCategorie (productID: Int): Flow< List<Product> >
