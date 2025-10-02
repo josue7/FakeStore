262c152c299590ee48b942defed9115d696e0aeb
Josue Marfil <josue.marfil@estudiante.uacm.edu.mx>
2025-10-01 21:31:38 -0600

    Se elimino categorías de DB
    Al inicio tenia pensado alamacenar en una entidad los productos favoritos por si se tenia pensado personalizar por usuario los gustos sin embargo replante el problema y concluí que con una sola tabla se puede gestionar esa parte

--- app/src/main/java/com/technical/practice/fakestore/data/repository/NetworkCategoryRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.technical.practice.fakestore.data.repository
-
-import com.technical.practice.fakestore.data.apiservice.FakeStoreApiService
-
-//class NetworkCategoryRepository (private val fakeStoreApiService: FakeStoreApiService): CategoryRepositoryNet {
-//    override suspend fun getAllCategories(): List<Category> = fakeStoreApiService.getCategories()
-//}
