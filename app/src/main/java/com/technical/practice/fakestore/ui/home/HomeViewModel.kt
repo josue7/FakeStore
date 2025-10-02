3209ad4dc4fe875614eb952e4b4c7fc19d4458fe
Josue Marfil <josue.marfil@estudiante.uacm.edu.mx>
2025-10-01 19:17:36 -0600

    .
    .

--- app/src/main/java/com/technical/practice/fakestore/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/technical/practice/fakestore/ui/home/HomeViewModel.kt
@@ -1,13 +1,8 @@
 package com.technical.practice.fakestore.ui.home
 
 import android.util.Log
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import coil.network.HttpException
-import com.technical.practice.fakestore.data.apiservice.ProductAPI
 import com.technical.practice.fakestore.data.database.product.Product
 import com.technical.practice.fakestore.data.repository.ProductRepositoryLocal
 import com.technical.practice.fakestore.data.repository.ProductRepositoryNet
@@ -18,8 +13,6 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import okhttp3.internal.ignoreIoExceptions
-import okio.IOException
 
 sealed interface HomeUiState {
     data class Success(val products: List<Product>) : HomeUiState
