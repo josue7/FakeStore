6757b3353a2b2863001a0a2da64035f797a79021
Josue Marfil <josue.marfil@estudiante.uacm.edu.mx>
2025-10-01 19:58:24 -0600

    favoritos
    quitar de favoritos

--- app/src/main/java/com/technical/practice/fakestore/ui/product/ProductFavoriteScreen.kt
+++ app/src/main/java/com/technical/practice/fakestore/ui/product/ProductFavoriteScreen.kt
@@ -1,10 +1,19 @@
 package com.technical.practice.fakestore.ui.product
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.technical.practice.fakestore.R
 import com.technical.practice.fakestore.data.database.product.Product
+import com.technical.practice.fakestore.ui.AppViewModelProvider
+import com.technical.practice.fakestore.ui.FakeStoreAppBar
 import com.technical.practice.fakestore.ui.navigation.NavigationDestination
+import com.technical.practice.fakestore.ui.theme.FakeStoreTheme
 
 object ProductFavoriteDestination: NavigationDestination {
     override val route = "product_favorite"
@@ -14,12 +23,17 @@ object ProductFavoriteDestination: NavigationDestination {
 @Composable
 fun ProductFavoriteScreen (
     listProducts: List<Product> = emptyList(),
-    navigateToView: (Int) -> Unit = {}
+    viewModel: ProductsFavoriteViewModel = viewModel(factory = AppViewModelProvider.Factory),
+    navigateToView: (Int) -> Unit = {},
+    modifier: Modifier = Modifier
 ) {
     ListProducts (
         products = listProducts,
-        onFavoriteToggle ={},
-        onProductClick = {}
+        onFavoriteToggle ={ productId ->
+            viewModel.onFavoriteToggle(productId)
+        },
+        onProductClick = {},
+        modifier = modifier
     )
 }
 
