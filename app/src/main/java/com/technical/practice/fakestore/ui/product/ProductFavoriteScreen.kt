502e29865958dffa4cec947f29cb23b57878c1da
Josue Marfil <josue.marfil@estudiante.uacm.edu.mx>
2025-10-01 19:59:42 -0600

    .
    .

--- app/src/main/java/com/technical/practice/fakestore/ui/product/ProductFavoriteScreen.kt
+++ app/src/main/java/com/technical/practice/fakestore/ui/product/ProductFavoriteScreen.kt
@@ -1,19 +1,13 @@
 package com.technical.practice.fakestore.ui.product
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.technical.practice.fakestore.R
 import com.technical.practice.fakestore.data.database.product.Product
 import com.technical.practice.fakestore.ui.AppViewModelProvider
-import com.technical.practice.fakestore.ui.FakeStoreAppBar
 import com.technical.practice.fakestore.ui.navigation.NavigationDestination
-import com.technical.practice.fakestore.ui.theme.FakeStoreTheme
 
 object ProductFavoriteDestination: NavigationDestination {
     override val route = "product_favorite"
