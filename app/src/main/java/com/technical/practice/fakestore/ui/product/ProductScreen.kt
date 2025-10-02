b082270576bda296d2326e4047c412eb029d95bc
Josue Marfil <josue.marfil@estudiante.uacm.edu.mx>
2025-10-01 19:07:31 -0600

    Adaptación para viewModel
    Se agrego los componentes faltantes com appTopBar para mostrar como pantalla independiente, además de tomar los datos del viewModel

--- app/src/main/java/com/technical/practice/fakestore/ui/product/ProductScreen.kt
+++ app/src/main/java/com/technical/practice/fakestore/ui/product/ProductScreen.kt
@@ -19,26 +19,69 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 import com.technical.practice.fakestore.R
 import com.technical.practice.fakestore.data.database.product.Product
+import com.technical.practice.fakestore.ui.AppViewModelProvider
+import com.technical.practice.fakestore.ui.FakeStoreAppBar
+import com.technical.practice.fakestore.ui.home.HomeBody
+import com.technical.practice.fakestore.ui.navigation.NavigationDestination
+import com.technical.practice.fakestore.ui.theme.FakeStoreTheme
 import java.util.Locale
+
+object ProductDestination: NavigationDestination {
+    override val route = "product"
+    override val titleRes = R.string.title_screen_product
+    const val PRODUCRCATEGORY = "category"
+    val routeWithArgs = "$route/{$PRODUCRCATEGORY}"
+}
+
+@Composable
+fun ProductScreen (
+    viewModel: ProductViewModel = viewModel(factory = AppViewModelProvider.Factory),
+    navigateToView: (Int) -> Unit = {}
+) {
+    FakeStoreTheme {
+        Scaffold (
+            topBar = {
+                FakeStoreAppBar(
+                    title = stringResource(ProductDestination.titleRes),
+                    modifier = Modifier,
+                    canNavigationBack = true
+                )
+            }
+        ) { innerPadding ->
+
+            ListProducts (
+                products = viewModel.products.collectAsState().value,
+                onFavoriteToggle = { productId ->
+                    viewModel.onFavoriteToggle(productId)
+                },
+                onProductClick = { },
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
+}
 
 @Composable
 fun ListProducts (
     products: List<Product>,
     onFavoriteToggle: (productId: Int) -> Unit,
-    onProductClick: (productId: Int) -> Unit
+    onProductClick: (productId: Int) -> Unit,
+    modifier: Modifier = Modifier
 ) {
     if (products.isEmpty()) {
         Text(
@@ -52,7 +95,8 @@ fun ListProducts (
         columns = GridCells.Fixed(2),
         contentPadding = PaddingValues(horizontal = 8.dp, vertical = 8.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
     ) {
         items(
             items = products,
@@ -131,34 +175,6 @@ fun CardProduct (
 }
 
 fun formatPrice (mount: Double) = String.format(Locale.US, "%.2f", mount)
-
-//@Composable
-//@Preview
-//fun CardProductPreview () {
-//    MaterialTheme {
-//        CardProduct(
-//            title = "Producto de Ejemplo",
-//            price = 29.995f,
-//             image = "...",
-//            isFavorite = true,
-//            onFavoriteClick = { }
-//        )
-//    }
-//}
-//
-//@Composable
-//@Preview
-//fun CardProductNotPreview () {
-//    MaterialTheme {
-//        CardProduct(
-//            title = "de Ejemplo",
-//            price = 43.993f,
-//            image = "...",
-//            isFavorite = false,
-//            onFavoriteClick = { }
-//        )
-//    }
-//}
 
 @Composable
 @Preview
